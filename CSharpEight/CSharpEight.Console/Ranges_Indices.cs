d6886d721ee22e3f302d961d1e5c55f55caf7958
James <sirtopas@gmail.com>
2019-09-25 21:06:32 +0100

    Added a slice
    Signed-off-by: James <sirtopas@gmail.com>

--- CSharpEight/CSharpEight.Console/Ranges_Indices.cs
+++ CSharpEight/CSharpEight.Console/Ranges_Indices.cs
@@ -4,15 +4,14 @@ namespace CSharpEight.Console
 {
     public class Ranges_Indices
     {
-        public string Indices()
+        public void Indices()
         {
             Index index1 = 3;  // number 3 from beginning
             Index index2 = ^4; // number 4 from end 
             int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             System.Console.WriteLine($"{a[index1]}, {a[index2]}"); // "3, 6"
             System.Console.WriteLine($"{a[(Index)3]}, {a[^4]}"); // "3, 6"
-
-            return $"{a[index1]}, {a[index2]}";
+            System.Console.WriteLine(a[index1..index2]); // { 3, 4, 5 }
         }
     }
 }
